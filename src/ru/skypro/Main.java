e1022591d529697f725b22e38ce8b22af1db05d0
Георгий <saygora@gmail.com>
2022-01-23 17:03:19 +0300

    ДЗ №8

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,5 +1,6 @@
 package ru.skypro;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
         return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
     }
 
-    private static void printIsLeapYearResult(int year) {
+    public static void printIsLeapYearResult(int year) {
         if (isLeapYear(year)) {
             System.out.println(year + " високосный год");
         } else {
@@ -18,34 +19,28 @@ public class Main {
     }
 
     ///Задача №2
-    public static int getDeviceOS(String name) {
-        if (name.equals("Android")) {
-            return 1;
-        } else {
-            return 0;
+    public static void printResultOS(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceIsOld) {
+            System.out.print("облегченную ");
         }
-    }
-
-    public static int getDeviceYear(int y) {
-        if (y >= 2015) {
-            return 1;
+        System.out.print("версию для ");
+        if (clientOS == 0) {
+            System.out.println("IOS");
         } else {
-            return 0;
+            System.out.println("Android");
         }
 
     }
 
-    public static void printResultOS(int a, int b) {
-        if (a == 0 && b == 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (a == 0 && b == 0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (a == 1 && b == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
     }
+
+
+
 
     ///Задача №3
     public static int getDeliveryDistance(int distance) {
@@ -71,8 +66,6 @@ public class Main {
                     System.out.println(j + " - символ дублируется с - " + chars[j]);
                     return;
                 }
-                if (chars[i] != chars[j]) {
-                }
             }
         }
         System.out.println("Дублей нет");
@@ -80,13 +73,18 @@ public class Main {
 
     ///Задача №5
     public static void getReverce(int[] num) {
-        for (int i = num.length - 1; i >= 0; i--) {
-            if (i != 0);
-            System.out.print(num[i] + " ");
+        int start = 0;
+        int end = num.length - 1;
+        while (start < end) {
+            int temp = num[start];
+            num[start++] = num[end];
+            num[end--] = temp;
+        }
+
         }
 
 
-    }
+
 
 
 
@@ -96,9 +94,7 @@ public class Main {
         printIsLeapYearResult(2008);
 
         ///Задача №2
-        int year = getDeviceYear(2008);
-        int osName = getDeviceOS("IOS");
-        printResultOS(year, osName);
+        printResultOS(0, 2015);
 
         ///Задача №3
         System.out.println("Дней на доставку - " + getDeliveryDistance(95));
@@ -110,6 +106,7 @@ public class Main {
         ///Задача №5
         int[] wum = new int[]{1, 2, 3, 4, 5, 6};
         getReverce(wum);
+        System.out.println(Arrays.toString(wum));
 
 
 
